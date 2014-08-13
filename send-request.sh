5af430243f9b8c1c4edc91d1880ea045753ef7ee
John Ryan <jnyryan@gmail.com>
2014-08-13 17:43:51 +0100

    initial commit

---
+++ send-request.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl https://vagrant-dc.contoso.com/adfs/services/trust/13/usernamemixed \
+-k \
+--data @request.txt \
+-H "Content-Type:application/soap+xml"  \
+--verbose | xml_pp
+#-o "output.txt"
+
