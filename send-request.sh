26c4e3eae6ef479f8c93c1aac7e233aba5c27a25
John Ryan <jnyryan@gmail.com>
2014-08-14 13:19:46 +0100

    working request to ADFS

--- send-request.sh
+++ send-request.sh
@@ -2,9 +2,8 @@
 
 curl https://vagrant-dc.contoso.com/adfs/services/trust/13/usernamemixed \
 -k \
---data @request.txt \
+--data @request.xml \
 -H "Content-Type:application/soap+xml"  \
 --verbose \
 | xml_pp \
 | tee "output.txt"
-
