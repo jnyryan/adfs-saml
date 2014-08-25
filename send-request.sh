e1728008db9a4d16f5836f136d70ade36d08e83d
John Ryan <jnyryan@gmail.com>
2014-08-25 15:21:14 +0100

    updated endpoint to dubdev.com

--- send-request.sh
+++ send-request.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+curl https://dubdevdc.dubdev.com/adfs/services/trust/13/usernamemixed \
+-k \
+--data @xml/request-token.xml \
+-H "Content-Type:application/soap+xml"  \
+--verbose \
+| tee "xml/response-token.xml"
+
+exit
+
 curl https://vagrant-dc.contoso.com/adfs/services/trust/13/usernamemixed \
 -k \
 --data @xml/request-token.xml \
